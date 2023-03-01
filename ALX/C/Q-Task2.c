cebc2aeb07499bcdb761558a927a52442e21578c
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-03-01 09:11:19 +0100

    Add files via upload

---
+++ ALX/C/Q-Task2.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+ 
+int main () {
+
+   int n[ 10 ]; /* n is an array of 10 integers */
+   int i,j;
+ 
+   /* initialize elements of array n to 0 */         
+   for ( i = 0; i < 10; i++ ) {
+      n[ i ] = i + 100; /* set element at location i to i + 100 */
+   }
+   
+   /* output each array element's value */
+   for (j = 0; j < 10; j++ ) {
+      printf("Element[%d] = %d\n", j, n[j] );
+   }
+ 
+   return 0;
+}
+//Pointers
+#include <stdio.h>
+
+int main () {
+
+   int  var1;
+   char var2[10];
+
+   printf("Address of var1 variable: %x\n", &var1  );
+   printf("Address of var2 variable: %x\n", &var2  );
+
+   return 0;
+}
+#include <stdio.h>
+
+int main () {
+
+   int  var = 20;   /* actual variable declaration */
+   int  *ip;        /* pointer variable declaration */
+
+   ip = &var;  /* store address of var in pointer variable*/
+
+   printf("Address of var variable: %x\n", &var  );
+
+   /* address stored in pointer variable */
+   printf("Address stored in ip variable: %x\n", ip );
+
+   /* access the value using the pointer */
+   printf("Value of *ip variable: %d\n", *ip );
+
+   return 0;
+}
+#include <stdio.h>
+
+int main () {
+
+   int  *ptr = NULL;
+
+   printf("The value of ptr is : %x\n", ptr  );
+ 
+   return 0;
+}
+//Strings
+#include <stdio.h>
+
+int main () {
+
+   char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+   printf("Greeting message: %s\n", greeting );
+   return 0;
+}
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+
+   char str1[12] = "Hello";
+   char str2[12] = "World";
+   char str3[12];
+   int  len ;
+
+   /* copy str1 into str3 */
+   strcpy(str3, str1);
+   printf("strcpy( str3, str1) :  %s\n", str3 );
+
+   /* concatenates str1 and str2 */
+   strcat( str1, str2);
+   printf("strcat( str1, str2):   %s\n", str1 );
+
+   /* total lenghth of str1 after concatenation */
+   len = strlen(str1);
+   printf("strlen(str1) :  %d\n", len );
+
+   return 0;
+}
