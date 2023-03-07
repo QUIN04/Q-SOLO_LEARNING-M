9ca5bc6eea7df6f3069e64343f9b042a6ea8df76
Quinuel Tabot  N-A <97244109+QUIN04@users.noreply.github.com>
2023-03-07 08:01:45 +0100

    NEWSLETTER POP-UP

---
+++ Day8.js
@@ -0,0 +1,8 @@
+//making a function which will toggle the class
+function toggleNewsletter(){
+  const newsletter = document.getElementById("newsletter");
+  newsletter.classList.toggle("active");
+
+
+
+}
