11e9e22234172d8f77b11f307a84b0a9f7fd814c
Quinuel Ndip-Agbor <97244109+QUIN04@users.noreply.github.com>
2023-04-07 23:47:11 +0100

    Create Day13.js

---
+++ JS/Day13.js
@@ -0,0 +1,8 @@
+//Iterate Through an Array with a For Loop
+// Setup
+var myArr = [ 2, 3, 4, 5, 6];
+var myTotal =0;
+// Only change code below this line
+for(var i = 0;i<= myArr.length; i++) {
+myTotal+=myArr[i];
+}
