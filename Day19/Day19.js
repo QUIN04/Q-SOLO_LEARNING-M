cefe3a0eb3a733d311006decded063deb903f68d
Quinuel Ndip-Agbor <97244109+QUIN04@users.noreply.github.com>
2023-04-11 20:23:35 +0100

    BMI CALCULATOR

---
+++ Day19/Day19.js
@@ -0,0 +1,15 @@
+const form = document.querySelector(".form").addEventListener(e.preventdefault();
+const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const result = document.querySelector(".result");
+
+  if (height === " " || height < 0 || isNaN(height)) {
+    result.innerHTML = "Please enter a valid input";
+  } else if (weight === " " || weight < 0 || isNaN(weight)) {
+    result.innerHTML = "Please enter a valid input";
+  } else {
+    const yourBMIis = (weight / ((height * height) / 10000)).toFixed(2);
+    result.innerHTML = `<span>${yourBMIis}</span>`;
+  }
+
+;
